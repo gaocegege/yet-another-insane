daf940c425ce333e3af0af47aa2f21d27b3febae
Gaoce <gaocegege@hotmail.com>
2015-05-11 23:16:52 +0800

    fix for print and so on

--- src/insane/CFG/CFGGeneration.scala
+++ src/insane/CFG/CFGGeneration.scala
@@ -925,9 +925,10 @@ trait CFGGeneration extends CFGTreesDef { self: AnalysisComponent =>
 
             stack push to
           case LOAD_MODULE(module) =>
-            val value = stack.pop
-            val index = stack.pop
-            val array = stack.pop
+            // what?
+            // val value = stack.pop
+            // val index = stack.pop
+            // val array = stack.pop
             stack push new CFG.ObjRef(module, module.tpe)
 
           case STORE_THIS(kind) =>
